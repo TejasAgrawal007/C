a70fa33b47fc9a3de782c494a9e9c8851aba019b
Mindspace <72118095+TejasAgrawal007@users.noreply.github.com>
2022-04-10 13:37:23 +0530

    Update 02_areaofCircule.c

--- Chapter_1/challenges/02_areaofCircule.c
+++ Chapter_1/challenges/02_areaofCircule.c
@@ -1,5 +1,5 @@
-// Caculate the Area of circule and modify the
-// same program to area of cylender to hard code output.
+// Calculate the Area of Circle and modify the
+// same program to area of Cylinder to hard code output.
 
 #include <stdio.h>
 
@@ -8,8 +8,8 @@ int main()
     int radius = 3;
     float pi = 3.14;
     int height = 3;
-    // printf("The area of circule is %f\n", pi*radius*radius);
-    printf("The area of cylender is %f\n", pi * radius * radius / height);
+    // printf("The area of Circle is %f\n", pi*radius*radius);
+    printf("The area of Cylinder is %f\n", pi * radius * radius / height);
     return 0;
 }
 
